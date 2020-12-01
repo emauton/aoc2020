afd73f400441b5459b9d0f191b3d91bd9473bb99
Margaret McGaley <mmcgaley@gmail.com>
2020-12-01 09:25:34 +0000

    Solution to day 1 part 1

--- src/aoc2020/day1.clj
+++ src/aoc2020/day1.clj
@@ -1,7 +1,23 @@
 (ns aoc2020.day1
-  (:require [aoc2020.util :as util]))
+  (:require [aoc2020.util :as util] 
+            [clojure.math.combinatorics :as c]))
+
+(defn indices-adding-to 
+  "Find the indices of two elements of nums that sum to target"
+  [nums target]
+  (let [combs (c/combinations (range (count nums)) 2)
+        test-sum (fn [[a b]] (= (+ (nth nums a) (nth nums b)) target))]
+    (first (filter test-sum combs))))
+
+(defn repair-expense-report
+  "Return the product of two elements of nums that sum to 2020"
+  [nums]
+  (let [[a b] (indices-adding-to nums 2020)]
+    (* (nth nums a) (nth nums b))))
 
 (defn main
-  "Day 1 of Advent of Code 2020: <insert title here>"
+  "Day 1 of Advent of Code 2020: Report Repair"
   [args]
-  (println "Day 1! Args:" args))
+  (println (->> (util/read-numbers "mags/input-1-1")
+                (repair-expense-report))) )
+
--- src/aoc2020/util.clj
+++ src/aoc2020/util.clj
@@ -11,6 +11,9 @@
   "Read line-by-line from a file in project root's resources/"
   (-> filename io/resource io/reader line-seq))
 
+(defn read-numbers [filename]
+  (map #(Integer/parseInt %) (line-resource filename)))
+
 (defn break-commas
   "Break up a line on commas"
   [line]
