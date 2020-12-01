9c104a0b1ed80ceea4c66d542672c58509f61d61
Margaret McGaley <mmcgaley@gmail.com>
2020-12-01 09:46:05 +0000

    Solution for day 1 part 2

--- src/aoc2020/day1.clj
+++ src/aoc2020/day1.clj
@@ -3,23 +3,23 @@
             [clojure.math.combinatorics :as c]))
 
 (defn indices-adding-to 
-  "Find the indices of two elements of nums that sum to target"
-  [nums target]
-  (let [combs (c/combinations (range (count nums)) 2)
-        test-sum (fn [[a b]] (= (+ (nth nums a) (nth nums b)) target))]
+  "Find the indices of n elements of nums that sum to target"
+  [nums n target]
+  (let [combs (c/combinations (range (count nums)) n)
+        test-sum (fn [indices] (= (apply + (map #(nth nums %) indices)) target))]
     (first (filter test-sum combs))))
 
 (defn repair-expense-report
   "Return the product of two elements of nums that sum to 2020"
-  [nums]
-  (let [[a b] (indices-adding-to nums 2020)]
-    (* (nth nums a) (nth nums b))))
+  [n nums]
+  (let [indices (indices-adding-to nums n 2020)]
+    (apply * (map #(nth nums %) indices))))
 
 (defn main
   "Day 1 of Advent of Code 2020: Report Repair
       lein run day1 <input>
    where <input> is a filename in project resources/"
-  [[filename]]
+  [[filename n]]
   (println (->> (util/read-numbers filename)
-                (repair-expense-report))))
+                (repair-expense-report (Integer/parseInt n)))))
 
