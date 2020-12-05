6d91008107b9b381638349196dd75694a1d3e63d
Cian Synnott <cian@emauton.org>
2020-12-05 11:57:55 +0000

    Day 5: cleanup

--- src/aoc2020/day5.clj
+++ src/aoc2020/day5.clj
@@ -3,28 +3,27 @@
 
 (defn get-row
   "Takes a string representation of a binary number in F (0) B (1) and returns an int"
-  [row-str] 
+  [row-str]
   (Integer/parseInt (apply str (map {\F \0 \B \1} row-str)) 2))
 
 (defn get-col
   "Takes a string representation of a binary number in F (0) B (1) and returns an int"
-  [col-str] 
+  [col-str]
   (Integer/parseInt (apply str (map {\R \1 \L \0} col-str)) 2))
 
 (defn parse-pass
-  "Create a boarding pass [row column id] from the input line 
-   F is front half, B is back half R is right, L is left 
+  "Get a boarding pass ID from the input line
+   F is front half, B is back half R is right, L is left
    eg FBFBBFFRLR gives row 44, column 5"
   [line]
   (let [row (get-row (subs line 0 7))
-        col (get-col (subs line 7 10))
-        id (+ (* row 8) col)]
-    [row col id]))
-    
+        col (get-col (subs line 7 10))]
+    (+ (* row 8) col)))
+
 (defn highest-id
   "Return the highest ID from the list of boarding passes [row column ID]"
   [passes]
-  (last (last (sort-by last passes))))
+  (last (sort passes)))
 
 (defn empty-seat
   "Find the id not present in passes but for which id + 1 and id -1 are both present"
@@ -33,10 +32,10 @@
             (if (= (inc a) b)
               b
               (reduced (inc a))))
-          (sort (map last passes))))
+          (sort passes)))
 
 (defn main
-  "Day 5 of Advent of Code 2020: Binary Boarding  
+  "Day 5 of Advent of Code 2020: Binary Boarding
       lein run day5 <input>
   where <input> is a filename in project resources/"
   [[filename]]
