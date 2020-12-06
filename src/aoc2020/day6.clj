ca74162d7e117153e31e77cb370b14981ea1f477
Margaret McGaley <mmcgaley@gmail.com>
2020-12-06 10:11:38 +0000

    Day 6 part 1

--- src/aoc2020/day6.clj
+++ src/aoc2020/day6.clj
@@ -1,6 +1,7 @@
 (ns aoc2020.day6
   (:require [aoc2020.util :as util]
-            [clojure.string :as s]))
+            [clojure.string :as s]
+            [clojure.set :as set]))
 
 (defn get-groups 
   "return a nested collection [group1 group2 ...] where each group is a
@@ -10,10 +11,18 @@
   [input]
   (map s/split-lines (s/split input #"\n\n")))
 
+(defn count-yes
+  "count how many questions were answered 'yes' by group. That is the cardinality of the
+   sets of questions answered 'yes' by the members of group. group is a collection of strings"
+  [group]
+  ;(count (reduce set/union (into #{} group))))
+  ;(apply set/union (map (into #{}) group)))
+  (count (apply set/union (map #(into #{} %) group))))
+
 (defn main
   "Day 6 of Advent of Code 2020: Custom Customs
       lein run day6 <input>
   where <input> is a filename in project resources/"
   [[filename]]
   (let [groups (get-groups (util/slurp-resource filename))]
-  (println "First group:" (first groups))))
+    (println "Sum of 'yes's over all groups: " (apply + (map count-yes groups)))))
