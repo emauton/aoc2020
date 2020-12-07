ca40fcde38a95990b573dc3fa25958eebbcb8feb
Margaret McGaley <mmcgaley@gmail.com>
2020-12-07 10:02:58 +0000

    Minor changes

--- src/aoc2020/day7.clj
+++ src/aoc2020/day7.clj
@@ -1,13 +1,27 @@
 (ns aoc2020.day7
   (:require [aoc2020.util :as util]
+            [clojure.core.memoize :as m]
             [clojure.string :as s]
             [clojure.set :as set]))
+
+(def example 
+  {"shiny gold" ["bright white" "muted yellow"]
+    "bright white" ["dark orange" "light red"]
+    "muted yellow" ["dark orange" "light red"]
+    "dark orange" []
+    "light red" []})
+
+(defn traverse
+  [found query]
+  (m/memo
+   (let [result (get example query)]
+     )))
 
 (defn main
   "Day 7 of Advent of Code 2020: Handy Haversacks 
       lein run day7 <input>
   where <input> is a filename in project resources/"
   [[filename]]
-  (let [groups (get-groups (util/slurp-resource filename))]
-    (println "Sum of 'yes's over all groups (union): " (apply + (map count-yes-union groups)))
-    (println "Sum of 'yes's over all groups (intersection): " (apply + (map count-yes-intersect groups)))))
+  (let [rules (util/read-lines filename)]
+    (println "First rule: " (first rules))
+    (println "First example: " (first example))))
