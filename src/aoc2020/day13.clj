afdf3c60279b0f6f5f72fbafed8be8cb9d498caf
Cian Synnott <cian@emauton.org>
2020-12-13 17:18:15 +0000

    Day 13: cleanup

--- src/aoc2020/day13.clj
+++ src/aoc2020/day13.clj
@@ -1,31 +1,36 @@
 (ns aoc2020.day13
-  (:require [aoc2020.util :as util] 
+  (:require [aoc2020.util :as util]
             [clojure.string :as s]
             [clojure.math.numeric-tower :as math]))
 
+;; Part 1
+
 (defn parse-buses
   [text]
-  (map #(Integer/parseInt %) (filter #(not= "x" %) (s/split text #","))))
+  (map #(Integer/parseInt %)
+       (filter #(not= "x" %) (s/split text #","))))
 
-(defn parse-schedule-gcd
-  [text]
-  (let [buses (s/split text #",")
-        indices (range (count buses))
-        pairs (map 
-               (fn [[bus index]] [(Integer/parseInt bus) index]) 
-               (filter #(not= "x" (first %)) (map vector buses indices)))
-        new-pairs (map (fn [[b i]] [b (cond 
-                        (= i 0) i
-                        (< i b) (- b i)
-                        :else (- b (mod i b)))]) pairs)]
-    [(map #(first %) new-pairs) (map #(second %) new-pairs)]))
-    ;(map (fn [[bus modifier]] [bus (- bus modifier)]) pairs)))
+(defn get-bus
+  [t buses]
+  (filter (fn [b] (not= nil b))
+          (map #(if (= 0 (rem t %)) [t %] nil) buses)))
+
+(defn next-bus
+  [earliest buses]
+  (let [times (drop earliest (range))]
+    (get-bus (first (drop-while (fn [t]
+                                  (empty? (get-bus t buses)))
+                                times))
+             buses)))
+
+;; Part 2
+
+;; CRT impl from https://rosettacode.org/wiki/Chinese_remainder_theorem#Clojure
 
 (defn extended-gcd
   "The extended Euclidean algorithm
   Returns a list containing the GCD and the Bézout coefficients
-  corresponding to the inputs. 
-   https://rosettacode.org/wiki/Chinese_remainder_theorem#Clojure"
+  corresponding to the inputs."
   [a b]
   (cond (zero? a) [(math/abs b) 0 1]
         (zero? b) [(math/abs a) 1 0]
@@ -41,10 +46,9 @@
                     (recur (- s0 (* q s)) s
                            (- t0 (* q t)) t
                            (- r0 (* q r)) r))))))
- 
-(defn chinese_remainder
-  " Main routine to return the chinese remainder 
-   https://rosettacode.org/wiki/Chinese_remainder_theorem#Clojure"
+
+(defn chinese-remainder
+  "Main routine to return the chinese remainder"
   [n a]
   (let [prod (apply * n)
         reducer (fn [sum [n_i a_i]]
@@ -56,15 +60,21 @@
                                                       ; (map vector n a) is same as
         ;                                             ; Python's version Zip (n, a)
     (mod sum-prod prod)))                             ; Result line
- 
-(defn get-bus
-  [t buses]
-  (filter (fn [b] (not= nil b)) (map #(if (= 0 (rem t %)) [t %] nil) buses)))
 
-(defn next-bus
-  [earliest buses]
-  (let [times (drop earliest (range))]
-    (get-bus (first (drop-while (fn [t] (empty? (get-bus t buses))) times)) buses)))
+(defn parse-schedule-crt
+  "Represent the bus schedule as the two pairs expected by CRT impl above:
+   a list of primes (buses) + the timestamp we're looking for mod that prime.
+   i.e. a system of congruences per
+   https://brilliant.org/wiki/chinese-remainder-theorem/"
+  [text]
+  (let [buses (s/split text #",")
+        indices (range (count buses))
+        pairs (map
+               (fn [[bus index]] [(Integer/parseInt bus) index])
+               (filter #(not= "x" (first %)) (map vector buses indices)))
+        congruences (map (fn [[b i]] [b (mod (- i) b)]) pairs)]
+    (apply map vector congruences))) ; "unzip" into a pair of lists
+                                     ; rather than a list of pairs
 
 (defn main
   "Day 13 of Advent of Code 2020: Shuttle Search
@@ -74,11 +84,9 @@
   (let [input (util/read-lines filename)
         earliest (Integer/parseInt (first input))
         buses (parse-buses (second input))
-        [t b] (first (next-bus earliest buses))
-        p (parse-schedule-gcd (second input)) ]
-    (println "earliest time:" earliest)
-    (println "buses:" buses)
-    (println "next bus:" (next-bus earliest buses))
-    (println "product:" (* b (- t earliest)))
-    (println "chinese remainer:" (chinese_remainder (first p) (second p)))))
-;    (println "Schedule match:" (get-timestamp (parse-schedule (second input))))))
+        [timestamp bus] (first (next-bus earliest buses))
+        [primes congruences] (parse-schedule-crt (second input))]
+    (println "bus number * schedule difference:"
+             (* bus (- timestamp earliest)))
+    (println "first timestamp for ordered bus departures:"
+             (chinese-remainder primes congruences))))
