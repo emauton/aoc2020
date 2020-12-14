79201c89b5f149350025eed140402ed29785207f
Margaret McGaley <mmcgaley@gmail.com>
2020-12-14 20:27:32 +0000

    Day 14 part 1

--- src/aoc2020/day14.clj
+++ src/aoc2020/day14.clj
@@ -2,32 +2,53 @@
   (:require [aoc2020.util :as util] 
             [clojure.string :as s]))
 
+(defn parse-mask
+  [mask]
+  ; reverse because it's most significant figure first
+  (let [indices (reverse (range (count mask)))]
+    (into [] (filter #(not= \X (second %)) (map vector indices (into [] mask))))))
+
 (defn parse-mem
   [line]
-  (if (not= \m (first line))
-    line
-    (s/split (subs line 4) #"] = ")))
-
-(parse-mem "mem[1292] = 29")
+  (if (s/includes? line "mem") 
+    (let [[mem val] (s/split (subs line 4) #"] = ")]
+      [mem (Integer/parseInt val)])
+    (parse-mask line)))
 
 (defn parse-instr
   "Parse input into a vector of [mask instrs &]"
   [input]
   (let [instr-set (map s/split-lines (s/split input #"mask = "))]
-    (mapv (fn [i-s] (mapv parse-mem i-s)) instr-set)))
-;  (map (fn [inst-set] (conj (map parse-mem (drop inst-set)) (first inst-set))) 
+    ; rest because s/split creates an empty vector for the nothing that comes before the
+    ; first mask
+    (rest (mapv (fn [i-s] (mapv parse-mem i-s)) instr-set))))
 
-(parse-instr "mask = 000001001011XX1XX100X0001011X0001101
-mem[54977] = 194579
-mem[29691] = 71157948
-mem[27205] = 122030256
-mem[43160] = 91267
-mem[45028] = 254793847
-mem[27137] = 1696
-mask = 1000011X10X10X1X1100101X00X011010001
-mem[20727] = 25071621
-mem[37927] = 626522009
-mem[4815] = 119068316")
+(defn apply-mask
+  "Apply the mask (first element of instrs) to each of the values in the pairs that
+   make up the rest of the list"
+  [instrs]
+  (let [mask (first instrs)
+        mem-list (rest instrs)]
+    (map (fn [[mem val]] 
+           [mem (reduce (fn [acc [index bit]] 
+                     (if (= bit \1) 
+                       (bit-set acc index)
+                       (bit-clear acc index))) 
+                   val mask)]) mem-list)))
+
+(defn execute-instrs
+  [input]
+  (let [masked (map apply-mask input)]
+    (reduce 
+     (fn [acc mem-list] 
+       (reduce 
+        (fn [inner-acc [key val]] (assoc inner-acc key val)) 
+        acc mem-list)) 
+     {} masked)))
+
+(defn sum-all
+  [value-map]
+  (reduce-kv (fn [acc _ val] (+ acc val)) 0 value-map))
 
 (defn main
   "Day 14 of Advent of Code 2020: Docking Data
@@ -35,4 +56,4 @@ mem[4815] = 119068316")
   where <input> is a filename in project resources/"
   [[filename]]
   (let [input (parse-instr (util/slurp-resource filename))]
-    (println (nth input 3))))
+    (println "sum:" (sum-all (execute-instrs input)))))
