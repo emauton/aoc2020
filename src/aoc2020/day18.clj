226d8e220bf096df505e1d8bfe75bbce5b0c3912
Margaret McGaley <mmcgaley@gmail.com>
2020-12-18 10:05:21 +0000

    Day 18 start

--- src/aoc2020/day18.clj
+++ src/aoc2020/day18.clj
@@ -1,5 +1,19 @@
 (ns aoc2020.day18
-  (:require [aoc2020.util :as util]))
+  (:require [aoc2020.util :as util] 
+            [clojure.string :as s]))
+
+
+(def test-str "4 * 9 * 3 + 2 + (8 + 5 * 8 * 5 * (4 * 8 * 8 * 8) + (5 + 6)) * 2")
+
+(defn expr
+  [symbols]
+  (let [[cur op & remainder] symbols
+        op-fn {\+ + \* *}]
+    (cond
+      (nil? nil) cur
+      (= op \)) cur
+      (= cur \() (recur (rest symbols))
+      :else ((op-fn op) cur (recur remainder)))))
 
 (defn main
   "Day 18 of Advent of Code 2020: Operation Order
