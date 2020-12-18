fae8a7bacdffd6d9be4f81c9b357c16f1cc1e68d
Cian Synnott <cian@emauton.org>
2020-12-18 11:25:27 +0000

    Day 18: some initial work, still broken

--- src/aoc2020/day18.clj
+++ src/aoc2020/day18.clj
@@ -2,22 +2,55 @@
   (:require [aoc2020.util :as util] 
             [clojure.string :as s]))
 
+(defn parse
+  [input]
+  (filter util/not-nil?
+          (map (fn [c]
+                 (case c
+                   \  nil
+                   \* :star
+                   \+ :plus
+                   \( :open
+                   \) :close
+                   (Integer/parseInt (str c))))
+               input)))
 
-(def test-str "4 * 9 * 3 + 2 + (8 + 5 * 8 * 5 * (4 * 8 * 8 * 8) + (5 + 6)) * 2")
+(defn compute-prefix
+  [syms]
+  (reduce (fn [[acc op] n]
+            (case n
+              :star [acc *]
+              :plus [acc +]
+              :open (reduced [acc op])
+              :close (reduced [acc op])
+              [(op acc n) +]))
+          [0 +]
+          syms))
 
 (defn expr
-  [symbols]
-  (let [[cur op & remainder] symbols
-        op-fn {\+ + \* *}]
-    (cond
-      (nil? nil) cur
-      (= op \)) cur
-      (= cur \() (recur (rest symbols))
-      :else ((op-fn op) cur (recur remainder)))))
+  [syms acc]
+  (let [[prefix op] (compute-prefix syms)
+        remain (drop-while #(not= :open %) syms)]
+    (if (empty? remain)
+      prefix
+      (recur (rest remain) prefix))
+
+    ))
+
+;(defn expr
+;  [[n op & remainder]]
+;  (let [op-fn {\+ + \* *}]
+;    (cond
+;      (nil? nil) n
+;      (= op \)) n 
+;      (= n \() (recur (rest symbols))
+;      :else ((op-fn op) n (recur remainder)))))
 
 (defn main
   "Day 18 of Advent of Code 2020: Operation Order
       lein run day18 filename"
   [[filename]]
-  (let []
-    (println "Part 1, sum:")))
+  (let [test-str "2 * 3 + 8 * (4 * 5)"
+        parsed (parse test-str)]
+    (println "parse" test-str)
+    (println "compute" (compute-prefix parsed))))
